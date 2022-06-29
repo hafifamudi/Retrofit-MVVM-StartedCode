7fa3d151d16c0c928c00cd4ab06911928663e540
hafifamudi <hafifcyber@gmail.com>
2022-06-30 01:01:17 +0800

    init files

---
+++ app/src/main/java/com/samawaenginerr/retrofitmvvm/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.samawaenginerr.retrofitmvvm.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.samawaenginerr.retrofitmvvm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
